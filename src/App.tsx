3e4ce09ed4525293bb5efb29b7a11639008d2fb8
Randell Bakker <randell@live.nl>
2026-04-29 19:07:08 +0200

    Add plant detail page with tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 import type { Plant } from "./types/plant.ts";
+import { useState } from "react";
 
 function App() {
+  type ActiveTab = "overview" | "timeline" | "chat";
+
+  const [activeTab, setActiveTab] = useState<ActiveTab>("overview");
+
   let plants: Plant[] = [
     {
       id: 1,
@@ -21,20 +26,53 @@ function App() {
   return (
     <>
       <h1>Project grow ai</h1>
+      <button
+        onClick={() => setActiveTab("overview")}
+        className={activeTab === "overview" ? "activeTab" : ""}
+      >
+        Overview
+      </button>
+      <button
+        onClick={() => setActiveTab("timeline")}
+        className={activeTab === "timeline" ? "activeTab" : ""}
+      >
+        Timeline
+      </button>
+      <button
+        onClick={() => setActiveTab("chat")}
+        className={activeTab === "chat" ? "activeTab" : ""}
+      >
+        Chat
+      </button>
+
       <section>
         {plants.map((plant) => (
           <div onClick={() => console.log(plant)} key={plant.id}>
             <div>{plant.name}</div>
             <div>{plant.species}</div>
-            {plant.history.map((item) => (
-              <div key={item.id}>
-                <div>{item.text}</div>
-                <div>{item.type}</div>
-                <div>{item.dateTime}</div>
-              </div>
-            ))}
           </div>
         ))}
+      </section>
+
+      <hr />
+      <section>
+        {activeTab === "overview" && (
+          <>
+            <div>{plants[0].name}</div>
+            <div>{plants[0].species}</div>
+          </>
+        )}
+
+        {activeTab === "timeline" &&
+          plants[0].history.map((item) => (
+            <div key={item.id}>
+              <div>{item.text}</div>
+              <div>{item.type}</div>
+              <div>{item.dateTime}</div>
+            </div>
+          ))}
+
+        {activeTab === "chat" && <div>Chat content</div>}
       </section>
     </>
   );
