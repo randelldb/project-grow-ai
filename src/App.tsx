31022870d8a84dc50f1efcb5bdb138f3174cdd02
Randell Bakker <randell@live.nl>
2026-04-29 15:20:18 +0200

    Create plant view list

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,37 @@ import "./App.css";
 import type { Plant } from "./types/plant.ts";
 
 function App() {
-  let plant1: Plant = {
-    id: 1,
-    name: "Tomato",
-    species: "Cherry",
-    history: [
-      {
-        id: 1,
-        text: "this is a text",
-        type: "user_message",
-        dateTime: "2020-03-01",
-      },
-    ],
-  };
+  let plants: Plant[] = [
+    {
+      id: 1,
+      name: "Tomato",
+      species: "Cherry",
+      history: [
+        {
+          id: 1,
+          text: "this is a text",
+          type: "user_message",
+          dateTime: "2020-03-01",
+        },
+      ],
+    },
+  ];
 
   return (
     <>
       <h1>Project grow ai</h1>
       <section>
-        {plant1.id}
-        {plant1.name}
-        {plant1.species}
-        {plant1.history.map((item) => (
-          <div key={item.id}>
-            <div>{item.text}</div>
-            <div>{item.type}</div>
-            <div>{item.dateTime}</div>
+        {plants.map((plant) => (
+          <div onClick={() => console.log(plant)} key={plant.id}>
+            <div>{plant.name}</div>
+            <div>{plant.species}</div>
+            {plant.history.map((item) => (
+              <div key={item.id}>
+                <div>{item.text}</div>
+                <div>{item.type}</div>
+                <div>{item.dateTime}</div>
+              </div>
+            ))}
           </div>
         ))}
       </section>
